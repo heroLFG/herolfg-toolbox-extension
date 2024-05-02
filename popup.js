8a048920bb733b3d51316e86bbb81595993ce2f1
Gavin Palmer <gavin@herolfg.com>
2024-05-02 07:47:50 -0500

    add close button

--- popup.js
+++ popup.js
@@ -1,4 +1,8 @@
-document.getElementById("sendMessage").addEventListener("click", function () {
+document.getElementById("closeButton").addEventListener("click", function () {
+    window.close();
+  });
+  
+  document.getElementById("sendMessage").addEventListener("click", function () {
     const message = document.getElementById("inputMessage").value;
     if (message.trim()) {
       const messageDiv = document.createElement("div");
